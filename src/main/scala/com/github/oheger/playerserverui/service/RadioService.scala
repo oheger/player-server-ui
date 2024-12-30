48b7e604eeaea38f795468ca8caf69ceef72e125
Oliver Heger <oliver.heger@oliver-heger.de>
2024-12-30 21:00:45 +0100

    Updated copyright headers for 2024.
    Signed-off-by: Oliver Heger <oliver.heger@oliver-heger.de>

--- src/main/scala/com/github/oheger/playerserverui/service/RadioService.scala
+++ src/main/scala/com/github/oheger/playerserverui/service/RadioService.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 Oliver Heger.
+ * Copyright 2023-2024-2024 Oliver Heger.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
